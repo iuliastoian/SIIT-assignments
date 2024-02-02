86dee93b93c529fddaa33c0601f7ea070ad574aa
iuliastoian <stoianiuliatia@gmail.com>
2024-02-02 11:22:52 +0200

    added assignment

---
+++ IuliaTiaStoian_ObfuscateEmail/IuliaTiaStoian_ObfuscateEmail/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IuliaTiaStoian_ObfuscateEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare and define input
+             string? input = Console.ReadLine();
+
+            if (input == null) { Console.WriteLine("Input must not be empty. Type an email address as input."); return; }
+
+            string substring = input.Split('@')[0];
+            string obfuscate = new String('*', substring.Length);
+            input = input.Replace(substring, obfuscate);
+
+            Console.WriteLine(input);
+        }
+    }
+}
