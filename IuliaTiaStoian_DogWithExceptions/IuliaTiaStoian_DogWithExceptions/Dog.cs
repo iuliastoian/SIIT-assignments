a192d10f05e887e864307c45a4d8fe1a167dd735
iuliastoian <stoianiuliatia@gmail.com>
2024-02-09 21:31:12 +0200

    added assignment

---
+++ IuliaTiaStoian_DogWithExceptions/IuliaTiaStoian_DogWithExceptions/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IuliaTiaStoian_DogWithExceptions
+{
+    internal class Dog
+    {
+        int _age;
+        string _name;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 0) throw new ApplicationException("The age cannot be a negative number.");
+                if (value > 100) throw new ApplicationException("The age cannot be greater than 100.");
+                _age = value;
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if ((value.Length < 2)) throw new ApplicationException("The name must contain at least 2 characters.");
+                _name = value;
+            }
+        }
+
+        public Dog(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+    }
+}
---
+++ IuliaTiaStoian_DogWithExceptions/IuliaTiaStoian_DogWithExceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IuliaTiaStoian_DogWithExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // negative age
+            try
+            {
+                Dog dog = new Dog(-5, "Rex");
+            }
+            catch (ApplicationException ex) { Console.WriteLine(ex.Message); }
+
+            // age greater than 100
+            try
+            {
+                Dog dog = new Dog(101, "Rex");
+            }
+            catch (ApplicationException ex) { Console.WriteLine(ex.Message); }
+
+            // name contains less than 2 characters
+            try
+            {
+                Dog dog = new Dog(2, "R");
+            }
+            catch (ApplicationException ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
